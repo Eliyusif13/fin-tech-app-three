d5afc0944fb8128030d67a749fc867617705e6e7
Sadiqov Eliyusif <alisadiqov20002@gmail.com>
2025-02-23 16:03:46 +0400

    first commit

---
+++ src/main/java/com/sadiqov/tech_app_three/TechAppThreeApplication.java
@@ -0,0 +1,13 @@
+package com.sadiqov.tech_app_three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAppThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechAppThreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sadiqov/tech_app_three/TechAppThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadiqov.tech_app_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAppThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
